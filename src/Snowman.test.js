07d79748acbe810363d5f2f9d03f2228c1bfc0e7
Nate Snyder <n8.snyder@berkeley.edu>
2022-03-02 12:34:59 -0800

    Trying to figure out testing

--- src/Snowman.test.js
+++ src/Snowman.test.js
@@ -1,17 +1,21 @@
 import { render, fireEvent } from "@testing-library/react";
 import Snowman from "./Snowman.js"
 
-it("the game should end if you lose (6 wrong guesses)", function() {]
-
-
-
-    const {container} = render(
+it("the game should end if you lose (6 wrong guesses)", function () {
+    const container = render(
         <Snowman
-        words={["test"]}
+            words={["test"]}
         />
     );
+    for (const letter of ["a", "b", "c", "d", "f", "g"]) {
+        fireEvent.click(container.getByText(letter));
+    }
 
-    expect(container.querySelector(".Snowman-letter-buttons")).not.toBeInTheDocument();
+    const wrongGuesses = container.getByText("Number of incorrect guesses: 6");
+    console.log(wrongGuesses);
+
+
+    // expect(container.getBy(".Snowman-letter-buttons")).not.toBeInTheDocument();
 
 })
 
